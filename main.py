e3559dccecc889d10bbdf492fc5d700890a97bd9
deathewillcome3 <45524556+deathewillcome3@users.noreply.github.com>
2021-04-20 01:28:54 -0400

    Everything works now except for file output
    Non pythonic second section, head hurts too much, will improve in later commit

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ import os
 import time
 import platform
 import tester
-
 from dotenv import load_dotenv
 load_dotenv()
-#If you use -f argument, loads configuration from file, otherwise uses defaults
+
+#If you use -f argument, loads configuration from file, otherwise uses defaults (If options arent fully specified default overrides)
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--filename", help="configuration file FIOTester should use")
 parser.add_argument("-d", "--device", help="device name FIOTester should write to")
 parser.add_argument("-n", "--test_name", help="names the test")
 args = parser.parse_args()
+with open("defaults.yml") as default:
+    defaults = yaml.safe_load(default)
+
 if args.filename and args.device and args.test_name:
     with open(args.filename) as file:
         config = yaml.safe_load(file)
-        tester.run_test_from_file(platform.system(), args.device, args.test_name, config)
+        tester.run_test_from_file(platform.system(), args.device, args.test_name, config, defaults)
 elif args.device and args.test_name:
-    defaults = yaml.safe_load("defaults.yml")
-    tester.run_test_from_file(platform.system(), args.device, args.test_name, args.filename)
-
+    tester.run_test_from_file(platform.system(), args.device, args.test_name, defaults, defaults)
 else:
     print(" Please select a device name using the -d flag and a test name using the -n flag")
 
--- tester.py
+++ tester.py
@@ -2,23 +2,38 @@ import itertools
 import subprocess
 from itertools import product
 
-def run_test_from_file(os, device, name, yaml_conf):
-    qdepths = yaml_conf.get("qdepths", "Item not Found")
-    threads = yaml_conf.get("threads", "Item not Found")
-    blocksizes = yaml_conf.get("blocksizes", "Item not Found")
-    readpcts = yaml_conf.get("readpcts", "Item not Found")
-    ioengine = yaml_conf.get("ioengine", "Item Not Found")
+
+def run_test_from_file(os, device, name, yaml_conf, defaults):
+    params = {param: value for (param, value) in defaults.items()}
+    params.update(yaml_conf)
+
+    def populate_args(arg, parent_file): return arg+str(parent_file.get(arg[2:-1])) if arg[2:-1] in parent_file \
+        else arg
+
+    arguments = ["--filename " + device, "--size ", "--thread ", "--direct ", "--rw ", "--refill_buffers ", "--norandommap",
+                 "--randrepeat ", "--ioengine ", "--bs ", "--rwmixread ", "--iodepth ", "--numjobs ", "--time_based ",
+                 "--runtime ", "--group_reporting ", "--name "]
     eqdepths = []
+    arguments = [populate_args(arg, params) for arg in arguments]
 
-    for eqdepth in product(qdepths, threads):
-        False if eqdepth[0]*eqdepth[1] in eqdepths else eqdepths.append(eqdepth[0]*eqdepth[1])
+    for eqdepth in product(params.get("qdepths"), params.get("threads")):
+        False if eqdepth[0] * eqdepth[1] in eqdepths else eqdepths.append(eqdepth[0] * eqdepth[1])
 
-    for coordinate in product(eqdepths, blocksizes, threads, qdepths, readpcts):
+    for coordinate in product(eqdepths, params.get("blocksizes"), params.get("threads"), params.get("qdepths"),
+                              params.get("readpcts")):
         if coordinate[2]*coordinate[3] == coordinate[0]:
-            command = []
-            if(os == "Windows"):
-                output = subprocess.run(["powershell.exe", "fio", "--help"] + command, capture_output=True)
+            arguments[9] += str(coordinate[1])
+            arguments[10] += str(coordinate[4])
+            arguments[11] += str(coordinate[3])
+            arguments[12] += str(coordinate[2])
+            arguments[len(arguments)-1] += str(coordinate[1])+"-"+str(coordinate[2])+"-"+str(coordinate[3])+"-"+\
+                                          str(coordinate[4])
+
+            print("Running this command" + str(arguments))
+            if os == "Windows":
+                output = subprocess.run(["powershell.exe", "fio", "--output-format=json"] + arguments, capture_output=
+                True)
             else:
-                output = subprocess.run(["fio", "--help"], capture_output=True)
+                output = subprocess.run(["fio", "--output-format=json"], capture_output=True)
 
     print(output)
