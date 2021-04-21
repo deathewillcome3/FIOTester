59afa95e3b1f3887453479d6aa96ef074a1c9668
Daniel Zhou <danieltianqizhou@gmail.com>
2021-04-21 15:42:12 -0400

    Linux version works now. Both Linux and Windows switched to pOpen instead of pRun. Still working on capturing output

--- tester.py
+++ tester.py
@@ -2,38 +2,34 @@ import itertools
 import subprocess
 from itertools import product
 
+def populate_args(arg, parent_file, head, tail):
+    return arg + str(parent_file.get(arg[head:tail])) if arg[head:tail] in parent_file \
+    else arg
 
 def run_test_from_file(os, device, name, yaml_conf, defaults):
     params = {param: value for (param, value) in defaults.items()}
     params.update(yaml_conf)
-
-    def populate_args(arg, parent_file): return arg+str(parent_file.get(arg[2:-1])) if arg[2:-1] in parent_file \
-        else arg
-
-    arguments = ["--filename " + device, "--size ", "--thread ", "--direct ", "--rw ", "--refill_buffers ", "--norandommap",
-                 "--randrepeat ", "--ioengine ", "--bs ", "--rwmixread ", "--iodepth ", "--numjobs ", "--time_based ",
-                 "--runtime ", "--group_reporting ", "--name "]
+    arguments = ["--filename=" + device, "--size=", "--direct=", "--rw=", "--refill_buffers", "--norandommap",
+                 "--randrepeat=", "--ioengine=", "--bs=", "--rwmixread=", "--iodepth=", "--numjobs=", "--time_based",
+                 "--runtime=", "--group_reporting", "--name=", "--output="]
     eqdepths = []
-    arguments = [populate_args(arg, params) for arg in arguments]
-
+    arguments = [populate_args(arg, params, 2, -1) for arg in arguments]
     for eqdepth in product(params.get("qdepths"), params.get("threads")):
         False if eqdepth[0] * eqdepth[1] in eqdepths else eqdepths.append(eqdepth[0] * eqdepth[1])
 
     for coordinate in product(eqdepths, params.get("blocksizes"), params.get("threads"), params.get("qdepths"),
                               params.get("readpcts")):
         if coordinate[2]*coordinate[3] == coordinate[0]:
-            arguments[9] += str(coordinate[1])
-            arguments[10] += str(coordinate[4])
-            arguments[11] += str(coordinate[3])
-            arguments[12] += str(coordinate[2])
-            arguments[len(arguments)-1] += str(coordinate[1])+"-"+str(coordinate[2])+"-"+str(coordinate[3])+"-"+\
-                                          str(coordinate[4])
-
+            arguments[8] += str(coordinate[1])
+            arguments[9] += str(coordinate[4])
+            arguments[10] += str(coordinate[3])
+            arguments[11] += str(coordinate[2])
+            arguments[len(arguments) - 1] += str(coordinate[1]) + "-" + str(coordinate[2]) + "-" + str(
+                coordinate[3]) + "-" + str(coordinate[4])
+            arguments[len(arguments) - 2] += str(coordinate[1]) + "-" + str(coordinate[2]) + "-" + str(
+                coordinate[3]) + "-" + str(coordinate[4])
             print("Running this command" + str(arguments))
             if os == "Windows":
-                output = subprocess.run(["powershell.exe", "fio", "--output-format=json"] + arguments, capture_output=
-                True)
+                output = subprocess.Popen(["powershell.exe", "fio", "--output-format=json", "--thread"] + arguments, cstdout=subprocess.PIPE, stderr=subprocess.PIPE)
             else:
-                output = subprocess.run(["fio", "--output-format=json"], capture_output=True)
-
-    print(output)
+                output = subprocess.Popen(["fio", "--output-format=json"] + arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
